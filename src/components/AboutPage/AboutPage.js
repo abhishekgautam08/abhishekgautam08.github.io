6ec68074a07187b192a1ad2a5d93fc70a02c975f
Abhishek <gautamabhishek0810@gmail.com>
2022-12-23 00:31:23 +0530

    fix: resume url

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -20,7 +20,7 @@ const AboutPage = () => {
             Progressive Web App (PWA), Service Workers, Push Notification etc.
           </p>
           <a
-            href="https://drive.google.com/file/d/1-eQJLWFPLApfn05-xbHbpgXhmyBtc-K9/view?usp=share_link"
+            href="https://drive.google.com/drive/folders/1rDsdNEElGXe9Q-J6C-q7P_Aqj8Xi8GqZ?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
